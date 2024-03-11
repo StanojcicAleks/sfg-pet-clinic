f4a2dd9bbafc3ae87ae9d14581bcab076af1cc51
Aleksandar Stanojcic <aleksandar.stanojcic@app-crafters.com>
2024-03-11 10:15:32 +0100

    Implemented index page and controller

--- sfg-pet-clinic/pet-clinic-data/src/main/java/org/example/sfgpetclinic/services/OwnerService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/org/example/sfgpetclinic/services/OwnerService.java
@@ -2,8 +2,6 @@ package org.example.sfgpetclinic.services;
 
 import org.example.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
 public interface OwnerService extends CrudService<Owner,Long> {
 
     Owner findByLastNAme(String lastName);
