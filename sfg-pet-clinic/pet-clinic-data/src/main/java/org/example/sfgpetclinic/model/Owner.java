098f72a4c89ab9cf743e3d4ba0fb6e8c29841776
Aleksandar Stanojcic <aleksandar.stanojcic@app-crafters.com>
2024-03-13 16:31:39 +0100

    Added visits

--- sfg-pet-clinic/pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/Owner.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/Owner.java
@@ -1,5 +1,16 @@
 package org.example.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
-    
+
+    private Set<Pet> sets;
+
+    public void setSets(Set<Pet> sets) {
+        this.sets = sets;
+    }
+
+    public Set<Pet> getSets() {
+        return sets;
+    }
 }
--- sfg-pet-clinic/pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/PetType.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package org.example.sfgpetclinic.model;
 
-public class PetType extends BaseEntity{
+public class PetType extends BaseEntity {
 
     private String name;
 
