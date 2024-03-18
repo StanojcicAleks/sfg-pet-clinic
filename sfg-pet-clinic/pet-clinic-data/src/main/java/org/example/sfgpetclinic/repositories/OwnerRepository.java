7a1ceeac694209420434150602cfddca7e390e88
Aleksandar Stanojcic <aleksandar.stanojcic@app-crafters.com>
2024-03-18 13:46:59 +0100

    Added repos

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/org/example/sfgpetclinic/repositories/OwnerRepository.java
@@ -0,0 +1,7 @@
+package org.example.sfgpetclinic.repositories;
+
+import org.example.sfgpetclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+}
